811df79679b97c2001c7762472155ceddf71cecb
MAZanabria <81774563+MAZanabria@users.noreply.github.com>
2023-12-01 23:32:09 -0400

    Add files via upload

---
+++ ExamenFinal/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div class="contenid">
+    <div class="header">
+            <div><img src="images/USFX.jpg" height="100px"></div>
+           <div>
+            <div id="menu" >
+                <div class="boton">Pregunta 1</div>
+                <div class="boton">Pregunta 2</div>
+                <div class="boton">Pregunta 3</div>
+                <div class="boton">Pregunta 4</div>
+                <div class="boton">Pregunta 5</div>
+          </div>
+          <div id="titulo">pregunta 1</div>
+           </div>
+          
+    </div> 
+
+       <div style="display: flex; min-height: 80%;">
+          <div id="sub-menu">opciones
+            <div class="boton2">detalle 1</div>
+            <div class="boton2">detalle 2</div>
+          </div>
+          <div id="contenido">
+            <div class="cont">
+                <div>SIS 256 Programacion Web</div>
+                <div>Examen Final-02-12-2023 7:am</div>
+            </div>
+            <div><img src="images/yo.jpg" height="100px"></div>
+            <div class="cont2">
+                <div>Maria Alejandra Zanabria Vega</div>
+                <div>Carrera: Ing. Sistemas</div>
+                <div>Repositorio Examen:</div>
+            </div>
+    
+        </div>
+       </div>
+    
+</div>
+<div id="pie">Sucre-Semestre 2-2023</div>
+
+</body>
+</html>
